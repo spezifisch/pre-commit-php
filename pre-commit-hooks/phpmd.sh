d62f3c201a056250d3457c3441d494d0c6296d99
Eagu Kim <egkim@arontier.co>
2020-07-30 07:40:35 +0900

    housekeeping

--- pre-commit-hooks/phpmd.sh
+++ pre-commit-hooks/phpmd.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-# Search PHP Mess Detector command
+# Set PHP Mess Detector command candidates
 vendor_command="vendor/bin/phpmd"
 system_command="phpmd"
 local_command="phpmd.phar"
 
+# Search PHP Mess Detector command
 exec_command=""
 if [ -f "$vendor_command" ]; then
   exec_command=$vendor_command
@@ -19,11 +20,18 @@ else
   echo "  * $local_command"
 fi
 
+# Collect input filenames and parameters
 report_param="$1"
 ruleset_param="$2"
 input_files="${@:3}"
 
 # Run PHP Mess Detector on each input file
+error_occurred=0
 for input_file in ${input_files[@]}; do
-  ${exec_command} "${input_file}" ${report_param} "${ruleset_param}"
+  echo "${exec_command} ${input_file} ${report_param} ${ruleset_params}"
+  "${exec_command} ${input_file} ${report_param} ${ruleset_param}"
+  if [ $? -ne 0 ]; then
+    error_occurred=1
+  fi
 done
+exit $error_occurred
