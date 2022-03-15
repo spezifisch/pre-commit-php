0a8791091c4ca0e3c4c80cba4e59cf0af638674a
Pascal <spezifisch@users.noreply.github.com>
2022-03-15 06:48:34 +0100

    don't output success, only failure. fix shellcheck complaints. add gh action

--- pre-commit-hooks/phpmd.sh
+++ pre-commit-hooks/phpmd.sh
@@ -11,31 +11,35 @@ phar_command="phpmd.phar"
 # Search PHP Mess Detector
 exec_command=""
 if [ -f "$vendor_command" ]; then
-  exec_command=$vendor_command
+    exec_command=$vendor_command
 elif hash $system_command 2>/dev/null; then
-  exec_command=$system_command
+    exec_command=$system_command
 elif [ -f "$phar_command" ]; then
-  exec_command="php $phar_command"
+    exec_command="php $phar_command"
 else
-  echo -e "${color_red}PHP Mess Detector is not found.${color_reset}"
-  echo "$vendor_command, $system_command, or $phar_command is requried."
-  exit 1
+    echo -e "${color_red}PHP Mess Detector is not found.${color_reset}"
+    echo "$vendor_command, $system_command, or $phar_command is requried."
+    exit 1
 fi
 
 # Run PHP Mess Detector
 report_param="$1"
 ruleset_param="$2"
-input_files="${@:3}"
+input_files=("${@:3}")
 error_occurred=0
 
-for input_file in ${input_files[@]}; do
-  command="${exec_command} ${input_file} ${report_param} ${ruleset_param}"
-  echo "Running: ${command}"
-  command_output=`eval $command`
-  echo "${command_output}"
-  if [[ "${command_output}" =~ ERROR ]] || [[ "${command_output}" =~ VIOLATION ]]; then
-    error_occurred=1
-  fi
-  echo
+for input_file in "${input_files[@]}"; do
+    command="${exec_command} ${input_file} ${report_param} ${ruleset_param}"
+    command_output=$(eval "$command")
+
+    # only print output when problems were found
+    if ! [[ "${command_output}" =~ "Found 0 violations and 0 errors" ]]; then
+        echo "Running: ${command}"
+        echo "${command_output}"
+        if [[ "${command_output}" =~ ERROR ]] || [[ "${command_output}" =~ VIOLATION ]]; then
+            error_occurred=1
+        fi
+        echo
+    fi
 done
 exit $error_occurred
